f24fcf15da3457108d3f3d65b00c8a6f17a95b02
MallocLu <luxiantao1998@163.com>
2022-03-12 21:44:42 +0800

    limit gpu

--- main.py
+++ main.py
@@ -167,7 +167,9 @@ def run_training():
 
         loss_align_reg, vs_train_op, vs_eval_op, offset_pred, loss_reg = model.construct_model()
         # Create a session for running Ops on the Graph.
-        sess = tf.Session()
+        # 占满100%的内存空间
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction = 0.9)
+        sess = tf.Session(config = tf.ConfigProto(gpu_options = gpu_options))
         # Run the Op to initialize the variables.
         init = tf.initialize_all_variables()
         sess.run(init)
